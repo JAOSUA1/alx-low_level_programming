116d2e5cd1f75799156326d7a616404a7d3f7989
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-04-08 19:16:02 -0700

    Last digit commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 if (lastDigit > 5)
 {
-printf("Last digit of n is %d and is greater than 5\n");
+printf("Last digit is %d and is greater than 5\n");
 }
 else if (lastDigit == 0)
 {
-printf("Last digit of n is %d and is 0\n");
+printf("Last digit is %d and is 0\n");
 }
 else if (lastDigit < 6 && 0)
 {
-printf("Last digit of n is %d and is less than 6 and not 0\n");
+printf("Last digit of is %d and is less than 6 and not 0\n");
 }
 return (0);
 }
