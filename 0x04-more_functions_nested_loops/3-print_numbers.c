4836108f279fab8e26e9fc7c8d59e0982af400f2
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-04-14 13:09:58 -0700

    Printing single digits alone commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - displays numbers from 0 to 9
+ * Return: 0 on success
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 1; c = 9; c++)
+	{
+		_putchar (c);
+		_putchar ('\n');
+	}
+}
