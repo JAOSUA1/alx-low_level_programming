c94b3dd3f9a3f2958765817ffd4f6c2438833472
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-01 07:30:05 -0700

    Printing x to power of y commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - prints the value of x raised to power of y
+ * @x: is the base number
+ * @y: the exponential
+ *
+ * Return: -1 if y is less than 0
+ * and 1 if y is 0 else returns x to power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -2,3 +2,4 @@ Recursion Begins readme
 Printing of strings readme
 Printing strings in reverse order readme
 Calculate and return factorial of a number readme
+Raise to power readme
