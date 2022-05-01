cee13a836c2d7f54a843b0b9aae6a0a04a3cf826
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-01 08:07:58 -0700

    Printing square root of a number commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: the number inputed
+ *
+ * Return: -1 for error and square root if appropriate
+ */
+
+int _sqrt_recursion(int n)
+{
+	int r;
+
+	if (r % (n / r) == 0)
+	{
+		if (r * (n / r) == n)
+			return (r);
+		else
+			return (-1);
+	}
+	else
+		return (0 + sqrt_recursion(n, r + 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,4 @@ Printing of strings readme
 Printing strings in reverse order readme
 Calculate and return factorial of a number readme
 Raise to power readme
+Square root readme
