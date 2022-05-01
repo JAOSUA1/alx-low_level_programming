e74188be0f2575bdedee3f298c8deda1fbe2a040
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-01 08:40:23 -0700

    Printing square root of a number commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,14 +7,17 @@
  * Return: -1 for error and square root if appropriate
  */
 
-int _sqrt_recursion(int n, int r)
+int _sqrt_recursion(int n)
 {
+	for (r >= 1; r++)
+	{
 	if (r % (n / r) == 0)
 	{
 		if (r * (n / r) == n)
 			return (r);
 		else
 			return (-1);
+	}
 	}
 	else
 		return (0 + sqrt_recursion(n, r + 1));
