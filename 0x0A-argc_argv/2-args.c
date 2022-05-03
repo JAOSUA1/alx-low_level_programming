3eccf8d0abc1aca3508e15fa704b2489c17f52b5
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-03 15:58:05 -0700

    Prints all arguments commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all the arguments passed
+ * @argc: stands for the number of arguments
+ * @argv: standa for the array of arguments passed
+ *
+ * Return: Always 0 on success
+ */
+
+int main(int argc, char *argv)
+{
+	int z;
+
+	for (z = 0; z < argc; z++)
+	{
+		printf("argv[%d] is %s\n", argv[z]);
+	}
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,3 +1,4 @@
 Arguments readme
 Program name readme
 Print number of arguments passed readme
+Print all arguments passed readme
