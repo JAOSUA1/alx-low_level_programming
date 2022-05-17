720f4355b970fb98d2df6899801f10387a7a0557
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-17 00:02:57 -0700

    Sum of arguments commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list lst;
+	va_list valist;
 	unsigned int a;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
-	va_start(lst, n);
+	va_start(valist, n);
 
 	for (a = 0; a < n; a++)
-		sum = sum + va_arg(lst, int);
-	va_end(lst);
+		sum = sum + va_arg(valist, int);
+	va_end(valist);
 	return (sum);
 }
