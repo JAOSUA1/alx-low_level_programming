1389f977c894d2738b855fdcc383b4f9a5ed0b59
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-18 06:04:06 -0700

    Sum all commit

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,4 +1,5 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 
 /**
  * sum_them_all - This program sums all its parameters
@@ -9,17 +10,17 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list valist;
+	va_list val;
 	unsigned int a;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
-	va_start(valist, n);
+	va_start(val, n);
 
 	for (a = 0; a < n; a++)
-		sum += va_arg(valist, int);
-	va_end(valist);
+		sum += va_arg(val, int);
+	va_end(val);
 	return (sum);
 
 	_putchar('\n');
