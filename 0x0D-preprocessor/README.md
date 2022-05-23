7bf415bff1779b281ada035cc5542ee620f01c69
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-23 11:43:58 -0700

    Filename commit

--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,3 +1,4 @@
 Preprocessor readme
 Macro header readme
 PI is set readme
+Print filename readme
---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,15 @@
+#ifndef MAIN
+#define MAIN
+#include <stdio.h>
+
+/**
+ * main - prints the name of file holding it
+ * Return: no return
+ */
+
+void main()
+{
+	printf("The file name of this program is %s\n", __FILE__);
+}
+
+#endif
