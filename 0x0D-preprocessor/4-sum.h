0d2e500b0b327033df178eab5068cfe019cee773
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-23 12:25:50 -0700

    Addition macro commit

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef ADD_IT
+#define ADD_IT
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUM(x, y) ((x) + (y))
+
+/**
+ * 4-sum - A macro that sets the sum of x and y
+ * Return: no return
+ */
+
+#endif
