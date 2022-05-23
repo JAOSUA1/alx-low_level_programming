3d9277db2c72e3ad7a81956b580a0e7056a30407
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-23 12:06:22 -0700

    Function likemacro commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCT
+#define FUNCT
+#define ABS(x) ((x) : ((x < 0) ? (x * (-1)))
+/**
+ * 3-function_like_macro - calculates the absolute value of x
+ * @x: the inputed value to be absolute
+ *
+ * Return: 0 on success
+ */
+
+#endif 
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -2,3 +2,4 @@ Preprocessor readme
 Macro header readme
 PI is set readme
 Print filename readme
+Function macro readme
