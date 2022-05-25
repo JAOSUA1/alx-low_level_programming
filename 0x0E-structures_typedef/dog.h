d77e7083a11448a8089d97ee43e358a8817d8c7b
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-05-25 03:05:58 -0700

    Dog structure commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,15 +4,22 @@
 #include <stdlib.h>
 
 /**
- * struct - makes a new structure type named dog
+ * dem - typedef for the structure
+ * struct dog - makes a new structure type named dog
+ * @name: name of the dog in char
+ * @age: Its age in float
+ * @owner: the name of the dog owner in char
+ *
  * Return: no return
  */
 
-typedef struct dog
+typedef struct dog dem;
+
+struct dog
 {
-	char *name
-	char *owner
-	float age
+	char *name;
+	char *owner;
+	float age;
 }
 
 #endif
