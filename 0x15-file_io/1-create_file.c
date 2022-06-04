4d7148230c444d16978e10edf26e327d67946e61
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-06-04 03:59:06 -0700

    Create file commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,10 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
 
 /**
  * create_file - creates a file
