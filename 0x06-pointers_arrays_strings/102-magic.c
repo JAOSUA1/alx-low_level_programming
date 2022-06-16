ba57fdcd92ac0348dc2b8cc35ad16c9021b65d90
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-06-16 08:34:25 -0700

    Magic number commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,8 +13,8 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-         * write your line of code here...
+	/**
+	 * write your line of code here...
 	 * Remember:
 	 * - you are not allowed to use a
 	 * - you are not allowed to modify p
@@ -24,8 +24,6 @@ int main(void)
 	p[5] = 98;
 	/* ...so that this prints 98\n */
 	p[5] = 98;
-
 	printf("a[2] = %d\n", a[2]);
-
 	return (0);
 }
