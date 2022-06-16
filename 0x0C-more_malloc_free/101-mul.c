87ce3c2f5beea31e3d7c46a4d78cda151824fdda
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-06-16 09:51:57 -0700

    Multiply them commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -18,13 +18,13 @@ void _is_zero(char *argv[])
 	if (argv[1][i] != '0')
 	{
 		isn1 = 0;
-			break;
+		break;
 	}
 	for (i = 0; argv[2][i]; i++)
 	if (argv[2][i] != '0')
 	{
 		isn2 = 0;
-			break;
+		break;
 	}
 	if (isn1 == 1 || isn2 == 1)
 	{
@@ -46,7 +46,7 @@ char *_initialize_array(char *ar, int lar)
 
 	for (c = 0; c < lar; c++)
 		ar[c] = '0';
-		ar[lar] = '\0';
+	ar[lar] = '\0';
 	return (ar);
 }
 
@@ -87,11 +87,11 @@ int main(int argc, char *argv[])
 	char *nout;
 
 	if (argc != 3)
-	printf("Error\n"), exit(98);
+		printf("Error\n"), exit(98);
 	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
-	printf("Error\n"), exit(98);
+		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
@@ -102,7 +102,7 @@ int main(int argc, char *argv[])
 			{
 				add = (nout[k] - '0') + addl;
 				if (add > 9)
-				nout[k - 1] = (add / 10) + '0';
+					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
 			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
@@ -122,5 +122,5 @@ int main(int argc, char *argv[])
 		}
 	}
 	printf("%s\n", nout);
-		return (0);
+	return (0);
 }
