349d47f07a2f18dcb5dadfed2b69693b10cacf6a
JAOSUA1 <akanji.joseph1011@gmail.com>
2022-06-17 15:45:04 -0700

    Initialize commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,7 +22,6 @@ typedef struct dog
 	float age;
 } dog_t;
 
-int main
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
